b9c56caf5d4bb4c5de6d7228e2886b55e30ff63a
Evan Jacobs <evan.jacobs@enigma.io>
2016-04-28 23:47:47 -0400

    Update dev deps & fix test for React v15

--- jest.preprocessor.js
+++
@@ -1,9 +0,0 @@
-const babelJest = require('babel-jest');
-
-module.exports = {
-    process: function(src, filename) {
-        return   filename.indexOf('node_modules') === -1
-               ? babelJest.process(src, filename)
-               : src;
-    }
-};
