0a07d692929b55e0cb6a5fd4ee7eaadc06a0d8cf
Evan Scott <probablyup@gmail.com>
2017-12-05 01:06:07 -0500

    fix up the site styles

--- site.js
+++ site.js
@@ -1,8 +1,50 @@
 import { darken, lighten, rgba } from 'polished';
-import { injectGlobal } from 'preact-emotion';
+import styled, { css, injectGlobal } from 'preact-emotion';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Markdown from './';
+
+class TryItLive extends React.PureComponent {
+    state = {markdown: document.getElementById('sample-content').textContent.trim()}
+
+    updateState = (e) => this.setState({markdown: e.target.value})
+
+    render () {
+        return (
+            <main>
+                <Header>
+                    <a
+                        target='_blank'
+                        href='https://github.com/yaycmyk/markdown-to-jsx'
+                        title='Check out the markdown-to-jsx source code'
+                        rel='noopener noreferrer'>
+                        <img src='./images/logo.svg' alt='markdown-to-jsx logo' />
+                    </a>
+
+                    <Description>
+                        <code>markdown-to-jsx</code> is a no-fuss compiler that takes Github-flavored Markdown (GFM)<br /> and forms it into renderable React content without the need for <code>dangerouslySetInnerHTML</code>.
+                    </Description>
+
+                    <LearnMore>
+                        See the <a target='_blank' href='https://github.com/yaycmyk/markdown-to-jsx/blob/master/README.md' rel='noopener noreferrer'>project README</a> for installation &amp; usage.
+                    </LearnMore>
+                </Header>
+
+                <Content>
+                    <Textarea
+                        onChange={this.updateState}
+                        value={this.state.markdown} />
+
+                    <Compiled>
+                        <Markdown className='content-compiled'>
+                            {this.state.markdown}
+                        </Markdown>
+                    </Compiled>
+                </Content>
+            </main>
+        );
+    }
+}
 
 const COLOR_ACCENT = '#77667D';
 const COLOR_BODY = '#444';
@@ -94,90 +136,53 @@ injectGlobal`
         padding: 0 3em;
         margin: 3em 0 0;
     }
+`;
 
-    header {
-        flex-shrink: 0;
-        margin-bottom: 2em;
-        text-align: center;
+const Header = styled.header`
+    flex-shrink: 0;
+    margin-bottom: 2em;
+    text-align: center;
 
-        img {
-            height: 100px;
-        }
-    }
-
-    .content {
-        display: flex;
-        flex-grow: 1;
-    }
-
-    .content-compiled,
-    .content-raw {
-        flex: 0 0 50%;
-        padding: 1em;
-    }
-
-    .content-compiled {
-        overflow: auto;
-        overflow-x: hidden;
-        margin-left: 1em;
-        max-height: 100vh;
-    }
-
-    .content-raw {
-        border-color: ${COLOR_ACCENT};
-        border-radius: 2px;
-        position: sticky;
-        top: 0;
-        font-family: 'Source Code Pro', Consolas, Monaco, monospace;
-        font-size: inherit;
-        margin-right: 1em;
-        max-height: 100vh;
-    }
-
-    .description {
-        font-size: 18px;
-    }
-
-    .learn-more {
-        color: ${lighten(0.3, COLOR_BODY)};
+    img {
+        height: 100px;
     }
 `;
 
-class TryItLive extends React.PureComponent {
-    state = {markdown: document.getElementById('sample-content').textContent.trim()}
+const Description = styled.p`
+    font-size: 18px;
+`;
 
-    updateState = (e) => this.setState({markdown: e.target.value})
+const LearnMore = styled.p`
+    color: ${lighten(0.3, COLOR_BODY)};
+`;
 
-    render () {
-        return (
-            <main>
-                <header>
-                    <a
-                        target='_blank'
-                        href='https://github.com/yaycmyk/markdown-to-jsx'
-                        title='Check out the markdown-to-jsx source code'
-                        rel='noopener noreferrer'>
-                        <img src='./images/logo.svg' alt='markdown-to-jsx logo' />
-                    </a>
+const sharedCss = css`
+    flex: 0 0 50%;
+    padding: 1em;
+`;
 
-                    <p className='description'><code>markdown-to-jsx</code> is a no-fuss compiler that takes Github-flavored Markdown (GFM)<br /> and forms it into renderable React content without the need for <code>dangerouslySetInnerHTML</code>.</p>
+const Content = styled.section`
+    display: flex;
+    flex-grow: 1;
+`;
 
-                    <p className='learn-more'>See the <a target='_blank' href='https://github.com/yaycmyk/markdown-to-jsx/blob/master/README.md' rel='noopener noreferrer'>project README</a> for installation &amp; usage.</p>
-                </header>
+const Textarea = styled.textarea`
+    ${sharedCss};
+    border-color: ${COLOR_ACCENT};
+    border-radius: 2px;
+    position: sticky;
+    top: 0;
+    font-family: 'Source Code Pro', Consolas, Monaco, monospace;
+    font-size: inherit;
+    margin-right: 1em;
+    max-height: 100vh;
+`;
 
-                <section className='content'>
-                    <textarea
-                        className='content-raw'
-                        onChange={this.updateState}
-                        value={this.state.markdown} />
-
-                    <Markdown className='content-compiled'>
-                        {this.state.markdown}
-                    </Markdown>
-                </section>
-            </main>
-        );
-    }
-}
+const Compiled = styled.div`
+    ${sharedCss};
+    overflow: auto;
+    overflow-x: hidden;
+    margin-left: 1em;
+`;
 
 ReactDOM.render(<TryItLive />, document.getElementById('root'));
