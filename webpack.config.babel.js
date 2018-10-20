c3686caff1998972d93ffecf2d9dba340f1fbdb3
Evan Jacobs <probablyup@gmail.com>
2018-10-20 14:06:37 -0500

    update some random dev shtuff

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,45 +1,48 @@
 import path from 'path';
 
 export default {
-    devServer: {
-        compress: true,
-        contentBase: path.resolve(__dirname, 'docs'),
-    },
-    devtool: 'inline-source-map',
-    entry: path.resolve(__dirname, 'site.js'),
-    mode: 'development',
-    module: {
-        rules: [{
-            exclude: /(node_modules)/,
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    babelrc: false,
-                    plugins: [
-                        'emotion',
-                    ],
-                    presets: [
-                        ['es2015', {
-                            'loose': true,
-                            'modules': false,
-                        }],
-                        'react',
-                        'stage-2',
-                    ],
+  devServer: {
+    compress: true,
+    contentBase: path.resolve(__dirname, 'docs'),
+  },
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, 'site.js'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        exclude: /(node_modules)/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            plugins: ['emotion'],
+            presets: [
+              [
+                'env',
+                {
+                  loose: true,
+                  modules: false,
                 },
-            },
-        }],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'docs'),
-    },
-    plugins: [],
-    resolve: {
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
+              ],
+              'react',
+              'stage-2',
+            ],
+          },
         },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  plugins: [],
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
     },
+  },
 };
