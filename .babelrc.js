4a0f8468c4d3298fa5aa3b7d6d993538547f7ff1
Evan Jacobs <probablyup@gmail.com>
2018-10-20 13:50:52 -0500

    minify the babel output

--- .babelrc.js
+++ .babelrc.js
@@ -1,19 +1,19 @@
-const plugins = [
-    'emotion',
-];
-
-if (process.env.NODE_ENV === 'production') {
-    plugins.push('transform-react-remove-prop-types');
-}
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    plugins,
-    presets: [
-        ['env', {
-            loose: true,
-            modules: process.env.BABEL_ENV === 'esm' ? false : 'commonjs',
-        }],
-        'react',
-        'stage-2',
+  plugins: ['emotion', prod && 'transform-react-remove-prop-types'].filter(
+    Boolean
+  ),
+  presets: [
+    [
+      'env',
+      {
+        loose: true,
+        modules: process.env.BABEL_ENV === 'esm' ? false : 'commonjs',
+      },
     ],
+    'react',
+    'stage-2',
+    prod && 'minify',
+  ].filter(Boolean),
 };
