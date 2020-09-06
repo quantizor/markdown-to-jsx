94b971043b6b44fde80424619b12fd427a3bafe5
Evan Jacobs <probablyup@gmail.com>
2020-09-05 23:34:33 -0400

    update jest-snapshot-html to remove excessive whitespace

--- index.component.spec.tsx
+++ index.component.spec.tsx
@@ -14,23 +14,19 @@ it('accepts markdown content', () => {
   render(<Markdown>_Hello._</Markdown>)
 
   expect(root.innerHTML).toMatchInlineSnapshot(`
-
-<em data-reactroot>
-  Hello.
-</em>
-
-`)
+    <em data-reactroot>
+      Hello.
+    </em>
+  `)
 })
 
 it('handles a no-children scenario', () => {
   render(<Markdown>{''}</Markdown>)
 
   expect(root.innerHTML).toMatchInlineSnapshot(`
-
-<span data-reactroot>
-</span>
-
-`)
+    <span data-reactroot>
+    </span>
+  `)
 })
 
 it('accepts options', () => {
@@ -47,12 +43,10 @@ it('accepts options', () => {
   )
 
   expect(root.innerHTML).toMatchInlineSnapshot(`
-
-<em data-reactroot>
-  Hello.
-</em>
-
-`)
+    <em data-reactroot>
+      Hello.
+    </em>
+  `)
 })
 
 it('merges className overrides, rather than overwriting', () => {
@@ -69,27 +63,23 @@ it('merges className overrides, rather than overwriting', () => {
   )
 
   expect(root.innerHTML).toMatchInlineSnapshot(`
-
-<pre data-reactroot>
-  <code class="lang-js foo">
-    foo
-  </code>
-</pre>
-
-`)
+    <pre data-reactroot>
+      <code class="lang-js foo">
+        foo
+      </code>
+    </pre>
+  `)
 })
 
 it('passes along any additional props to the rendered wrapper element', () => {
   render(<Markdown className="foo"># Hello</Markdown>)
 
   expect(root.innerHTML).toMatchInlineSnapshot(`
-
-<h1 data-reactroot
-    id="hello"
-    class="foo"
->
-  Hello
-</h1>
-
-`)
+    <h1 data-reactroot
+        id="hello"
+        class="foo"
+    >
+      Hello
+    </h1>
+  `)
 })
