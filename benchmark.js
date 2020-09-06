4913fc31186731ab7dd1a9c696e2dccc1750ac1d
Evan Jacobs <probablyup@gmail.com>
2020-09-06 02:00:27 -0400

    add a progress bar to the benchmark

--- benchmark.js
+++ benchmark.js
@@ -11,6 +11,11 @@ const suite = new BenchTable();
 
 const fixture = fs.readFileSync('./fixture.md', 'utf8');
 
+const bar = new cliProgress.SingleBar({
+  clearOnComplete: true
+}, cliProgress.Presets.shades_classic)
+let totalCycles
+
 // add tests
 suite
   .addFunction('markdown-to-jsx', input => compiler(input))
@@ -19,6 +24,9 @@ suite
   .addFunction('markdown-it', input => mdIt.render(input))
   .addInput('simple markdown string', ['_Hello_ **world**!'])
   .addInput('large markdown string', [fixture])
+  .on('start', () => { totalCycles = suite._counter; bar.start(totalCycles, 0) })
+  .on('cycle', () => bar.update(totalCycles - suite._counter))
+  .on('abort error complete', () => bar.stop())
   .on('complete', function() {
     console.log('Fastest is ' + suite.filter('fastest').map('name'));
     console.log(suite.table.toString());
