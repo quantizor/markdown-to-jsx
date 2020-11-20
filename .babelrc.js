66fb9b38b437271f2db7609b470e3272d872b3dd
Evan Jacobs <probablyup@gmail.com>
2020-11-20 13:53:21 -0500

    remove now-unnecessary babel config

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-// remove when https://github.com/developit/microbundle/pull/702 is released
-module.exports = {
-  plugins: [
-    '@babel/plugin-transform-typescript',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
-}
