db855b378ee6dbf31653db0cb61a40f5f18c6db6
Paul Edwards <paule@mediamolecule.com>
2020-11-20 11:10:21 -0600

    Update tests to cover new option

--- index.compiler.spec.tsx
+++ index.compiler.spec.tsx
@@ -3253,3 +3253,20 @@ it('handles a holistic example', () => {
 
   expect(root.innerHTML).toMatchSnapshot()
 })
+
+it('should not convert bare urls to anchors if disableParsingBareUrls option is set', () => {
+  render(
+    compiler(
+      'Please leave me alone https://github.com/probablyup/markdown-to-jsx',
+      {
+        disableParsingBareUrls: true,
+      }
+    )
+  )
+
+  expect(root.innerHTML).toMatchInlineSnapshot(`
+    <span>
+      Please leave me alone https://github.com/probablyup/markdown-to-jsx
+    </span>
+  `)
+})
