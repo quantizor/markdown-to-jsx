8f84a269076d494c9593a263ff43d5c7f2b240f8
Evan Jacobs <probablyup@gmail.com>
2023-12-30 13:56:42 -0500

    chore: fix benchmark, update comparison library

--- benchmark.js
+++ benchmark.js
@@ -3,7 +3,7 @@ import cliProgress from 'cli-progress'
 import * as fs from 'fs'
 import SimpleMarkdown from 'simple-markdown'
 import MarkdownIt from 'markdown-it'
-import { compiler } from './dist'
+import { compiler } from './dist/index.module.js'
 
 const mdIt = new MarkdownIt()
 const suite = new BenchTable()
